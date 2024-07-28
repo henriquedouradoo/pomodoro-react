b5748e74516bdd3be581de5610b62d4bb75b07c6
Henrique Dourado Domingos <henriquedsenai@gmail.com>
2024-07-28 18:19:03 -0300

    Estrutura da aplicação e Styled Components

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "./components/Button";
+
+export function App() {
+
+  return (
+    <>
+    <Button variant="primary" />
+    <Button variant="secundary" />
+    <Button variant="danger" />
+    <Button variant="success" />
+    <Button />
+    </>
+  )
+}
+
+
