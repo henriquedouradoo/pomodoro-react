86e450116f7724efd60f9ad52c4f0a8f8541408e
Henrique Dourado Domingos <henriquedsenai@gmail.com>
2024-07-30 23:33:20 -0300

    Aprimorando inputs

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -46,6 +46,10 @@ const BaseInput = styled.input`
 export const TaskInput = styled(BaseInput)`
   flex: 1;
   font-size: 0.94rem;
+
+  &::-webkit-calendar-picker-indicator {
+    display: none !important;
+  }
 `
 
 export const MinutesAmountInput = styled(BaseInput)`
